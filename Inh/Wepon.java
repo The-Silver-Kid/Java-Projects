3b33716b85af846499311d103468b7235aed5ff2
Reggie Peck <soulsilver771@gmail.com>
2015-03-04 09:51:48 -0700

    Finish Wepon Class :+1:

--- Inh/Wepon.java
+++ Inh/Wepon.java
@@ -10,4 +10,16 @@ public class Wepon {
 		req = 150;
 		current = 0;
 	}
+	
+	public void dDealt(float def, float cH) {
+		int z = damage - def;
+		if (z <= 0) {
+			z = 0;
+		}
+		cH = cH - z;
+		System.out.println("You dealt " + z + " damage!");
+		System.out.println("Remaining Health: " + cH + "!");
+		System.out.println(name + " Gains 1 XP!")
+		current = current + 1;
+	}
 }

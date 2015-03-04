e06cd345e8fc3c4509631e0b85816941f8576a99
Reggie Peck <soulsilver771@gmail.com>
2015-03-04 09:42:03 -0700

    Begin work on Wepon Class

--- Inh/Wepon.java
+++ Inh/Wepon.java
@@ -1,5 +1,13 @@
-
-
 public class Wepon {
+	private float damage;
+	private int level, req, current;
+	private String name;
 	
+	public void adToInventorii(String tS) {
+		name = tS;
+		damage = 2;
+		level = 1;
+		req = 150;
+		current = 0;
+	}
 }

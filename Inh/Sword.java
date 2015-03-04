7b89eb1b17cf8e7919109904a8741f3e80fbd46c
Reggie Peck <soulsilver771@gmail.com>
2015-03-04 09:56:24 -0700

    Finish Sword.java :fire:

--- Inh/Sword.java
+++ Inh/Sword.java
@@ -1,5 +1,22 @@
 
 
 public class Sword extends Wepon {
+	private Boolean pierce = false;
+	private int sharp;
 	
+	public void sDealt(float def, float cH) {
+		if (!pierce) {
+			int z = damage + sharp - def;
+			if (z <= 0) {
+				z = 0;
+			}
+		} else {
+			z = damage + sharp;
+		}
+		cH = cH - z;
+		System.out.println("You dealt " + z + " damage!");
+		System.out.println("Remaining Health: " + cH + "!");
+		System.out.println(name + " Gains 1 XP!")
+		current = current + 1;
+	}
 }

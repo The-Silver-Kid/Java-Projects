51404d1c5e47ef58e20c8cda595e6a64ccc3c467
The-Silver-Kid <regpuppy77@gmail.com>
2015-05-08 16:05:30 -0600

    pizza

---
+++ src/DevTSK/Random_Projects/Order.java
@@ -0,0 +1,21 @@
+package DevTSK.Random_Projects;
+
+public class Order{
+	public static void main(String args[]) {
+		String pizzaNames[] = new String[4];
+		int pizza[] = new int[4];
+		int breadsticks = 2;
+		
+		int bs = breadsticks;
+		
+		pizza[0] = 1;
+		pizza[1] = 1;
+		pizza[2] = 1;
+		pizza[3] = 2;
+		
+		pizzaNames[0] = "Hawaian";
+		pizzaNames[1] = "Deep dish thing";
+		pizzaNames[2] = "Sausage";
+		pizzaNames[3] = "Peperonii";
+	}
+}

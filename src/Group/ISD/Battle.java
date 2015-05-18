90da58958da7d0bbf25695db6696d095c8faf2d7
Reggie Peck <soulsilver771@gmail.com>
2015-05-18 10:05:43 -0600

    add group project from school to public :fire:

---
+++ src/Group/ISD/Battle.java
@@ -0,0 +1,21 @@
+package ISD;
+
+public class Battle{
+	private Badii badone;
+	private int playerhealth;
+	
+	
+	public Battle(Badii g, int h) {
+		badone = g;
+		playerhealth = h;
+	}
+	
+	public boolean start() {
+		while ((playerhealth >= 1) || (badone.he() >= 1)){
+			
+		}
+		
+		return false;
+	}
+	
+}

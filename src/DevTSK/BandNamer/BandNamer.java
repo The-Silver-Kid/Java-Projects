3a07732d5b08896d2384dc1bb8f80357edb47443
The-Silver-Kid <regpuppy77@gmail.com>
2015-06-01 18:36:53 -0600

    k

--- src/DevTSK/BandNamer/BandNamer.java
+++ src/DevTSK/BandNamer/BandNamer.java
@@ -1,5 +1,5 @@
 /* Gabe Hill working on the file IO for the words
- * Zack Arbanas is working on collecting and organizing words to use
+ * Zack Arbanas is working on collecting and organising words to use
  * The_Silver_Kid overall project
  */
 
@@ -8,40 +8,117 @@ package DevTSK.BandNamer;
 
 import java.io.*;
 import java.net.*;
-//import java.nio.*;
 
+import javax.swing.*;
 
-
+import java.awt.*;
+import java.awt.event.*;
 
 public class BandNamer {
-
-	public static void main(String[] args){
-		String end;
-		int n, v, av, aj;
-		
-		
-		String noun = getLst("/images/nounList.bnn");
-		String verb = getLst("/images/verbList.bnn");
-		String adv = getLst("/images/advList.bnn");
-		String adj = getLst("/images/adjList.bnn");
-		
-		String[] nouns = noun.split("\\s+");
-		String[] verbs = verb.split("\\s+");
-		String[] adverbs = adv.split("\\s+");
-		String[] adjectives = adj.split("\\s+");
-		
-		System.out.println();
+	
+	public static String[] nouns;
+	public static String[] verbs;
+	public static String[] adverbs;
+	public static String[] adjectives;
+	
+	public JFrame bandNamer;
+	public static JLabel output;
+	public static JButton get;
+	public static JButton neo;
+	public static JLabel bld;
+	public static JLabel pcnt;
+	public static JProgressBar bar;
+	
+	private final Action getResources = new SwingAction(); 
+	private final Action getNeoName = new SwingAction2();
+	
+	private BandNamer(){
+		init();
 	}
 	
-	public static String getLst(String path){
+	private void init(){
+		bandNamer = new JFrame();
+		bandNamer.getContentPane().setBackground(SystemColor.window);
+		bandNamer.setIconImage(Toolkit.getDefaultToolkit().getImage(BandNamer.class.getResource("/images/ikon.png")));
+		bandNamer.setTitle("Band Naminator");
+		bandNamer.setBackground(SystemColor.window);
+		bandNamer.setResizable(false);
+		bandNamer.setBounds(0, 0, 400, 300);
+		bandNamer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bandNamer.getContentPane().setLayout(null);
+		
+		bar = new JProgressBar();
+		bar.setValue(100);
+		bar.setBounds(0, 253, 394, 20);
+		bar.setForeground(SystemColor.windowText);
+		bar.setBackground(SystemColor.window);
+		bar.setStringPainted(true);
+		bandNamer.getContentPane().add(bar);
+		
+		bld = new JLabel();
+		
+		pcnt = new JLabel();
+		
+		output = new JLabel();
+		
+		get = new JButton();
+		get.setAction(getResources);
+		
+		neo = new JButton();
+		neo.setAction(getNeoName);
+		
+	}
+	
+	public static void main(String[] args){
+		
+		BandNamer window = new BandNamer();
+		window.bandNamer.setVisible(true);
+		
+		/*String end = "";
+		int n, v, av=0, aj;
+		
+		av = av + 1;
+		
+		System.out.println("Loading Resources:\nNouns...");
+		String noun = getLst("/images/nounList.bnn", 90963);
+		System.out.println("Verbs...");
+		String verb = getLst("/images/verbList.bnn", 30802);
+		//System.out.println("Adverbs...");
+		//String adv = getLst("/images/advList.bnn", 6276);
+		System.out.println("Adjectives...");
+		String adj = getLst("/images/adjList.bnn", 28479);
+		System.out.println("Getting name...");
+		
+		String[] nouns = noun.split("\\r?\\n");
+		String[] verbs = verb.split("\\r?\\n");
+		//String[] adverbs = adv.split("\\r?\\n");
+		String[] adjectives = adj.split("\\r?\\n");
+		
+		n = (int) (Math.random() * nouns.length);
+		v = (int) (Math.random() * verbs.length);
+		//av = (int) (Math.random() * adverbs.length);
+		aj = (int) (Math.random() * adjectives.length);
+		
+		end = adjectives[aj] + " " + nouns[n] + " " + verbs[v];
+		
+		System.out.println(end);*/
+	}
+	
+	public static String getLst(String path, double t){
 		String temp = "",s;
+		double i = 0.0;
 		URL uri = BandNamer.class.getResource(path);
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(uri.getPath()));
 		
 			while ((s = br.readLine()) != null) {
-				System.out.print(s);
-				temp = temp + s;
+				i = i+1;
+				System.out.println(Math.round(100.0*(i/t)) + "%");
+				if (i == 1){
+					temp = temp + s;
+				} else { 
+					temp = temp + "\n" + s;
+				}
 			}
 		
 			br.close();
@@ -50,5 +127,30 @@ public class BandNamer {
 		}
 		return temp;
 	}
+	
+	private class SwingAction extends AbstractAction{
+		private static final long serialVersionUID = -6534894326484781815L;
+		
+		public SwingAction() {
+			putValue(NAME, "Generate Libs");
+			putValue(SHORT_DESCRIPTION, "Generates Required String[]s.");
+		}
 
+		public void actionPerformed(ActionEvent arg0) {	
+			
+		}
+	}
+	
+	private class SwingAction2 extends AbstractAction{
+		private static final long serialVersionUID = -4439810172513433428L;
+
+		public SwingAction2() {
+			putValue(NAME, "Get New Name");
+			putValue(SHORT_DESCRIPTION, "Generates new name.");
+		}
+
+		public void actionPerformed(ActionEvent arg0) {	
+			
+		}
+	}
 }
--- src/DevTSK/Char/CharRP.java
+++ src/DevTSK/Char/CharRP.java
@@ -112,8 +112,9 @@ public class CharRP {
 	public static Ponii zane = new Ponii("Zane Fireshine", "\nStallion\nUnicorn\nBlack coloured\nDosent like leaving the ground\nBlack and Green Mane\nGigaplex Leader", "\nA Sandy coloured crossed box", "Solar Flare", "Earth Quakes", 20, "/images/zane.png");
 	public static Ponii axel = new Ponii("Axel Fireshine", "\nStallion\nUnicorn\nA bright orange colour\nCan stand in intence flames and come out unharmed\nmane of fire\nLight Industries member", "\nfireball", "Solar Flare", "Earth Quakes", 18, "/images/axel.png");
 	public static Ponii dark = new Ponii("Dark Rainbow", "\nStallion\nAlicorn\nGrey coloured\nHe is an alicorn but only looks like a pegasus because laws and crap\nRainbow like Rainbow Dash's", "\nan atom", "Franda", "Slaziur", "Rainbow Dash", 20, 0, "/images/DR.png");
-	public static Ponii c418 = new Ponii("C418 (C4)", "\nStallion\nBlack colour\nMachine ponii built by radon that Looks almost exactly like him...", "\nDosent have one as he is a machine\nMane is the same as radon's only green instead of yellow", "N/A", "Shadow Radon", "C523", 5, 0, "/images/null.png");
+	public static Ponii c418 = new Ponii("C418 (C4)", "\nStallion\nBlack colour\nMachine ponii built by radon that Looks almost exactly like him...", "\nDosent have one as he is a machine\nMane is the same as radon's only green instead of yellow", "N/A", "Shadow Radon", "C523", 5, 0, "/images/C4.png");
 	public static Ponii c523 = new Ponii("C523 (C5)", "\nMare\nCream sort of colour\nAnother machine ponii built by radon however she was built for C4 to have a friend\nHas the same coloured mane as AJ", "\nDosent have one because she is a machine", "N/A", "Shadow Radon", "C418", 3, 0, "/images/null.png");
+	
 	
 	//Cannon poniis
 	public static Ponii SS = new Ponii("Sunset Shimmer", "\nMare\nUnicorn\nYou know from EQG?", "\nThe yellow and red sun thing", "?", "?", 19, "/imgages/null.png");

e92b975d53af343aaaadd330b6c573a89dc8793e
The-Silver-Kid <regpuppy77@gmail.com>
2015-06-02 18:19:27 -0600

    Ugh

--- src/DevTSK/Char/CharRP.java
+++ src/DevTSK/Char/CharRP.java
@@ -105,8 +105,8 @@ public class CharRP {
 	public static Ponii axel = new Ponii("Axel Fireshine", "\nStallion\nUnicorn\nA bright orange colour\nCan stand in intence flames and come out unharmed\nmane of fire\nLight Industries member", "\nfireball", "Solar Flare", "Earth Quakes", 18, "/images/axel.png");
 	public static Ponii dark = new Ponii("Dark Rainbow", "\nStallion\nAlicorn\nGrey coloured\nHe is an alicorn but only looks like a pegasus because laws and crap\nRainbow like Rainbow Dash's", "\nan atom", "Franda", "Slaziur", "Rainbow Dash", 20, 0, "/images/DR.png");
 	public static Ponii c418 = new Ponii("C418 (C4)", "\nStallion\nBlack colour\nMachine ponii built by radon that Looks almost exactly like him...", "\nDosent have one as he is a machine\nMane is the same as radon's only green instead of yellow", "N/A", "Shadow Radon", "C523", 5, 0, "/images/C4.png");
-	public static Ponii c523 = new Ponii("C523 (C5)", "\nMare\nCream sort of colour\nAnother machine ponii built by radon however she was built for C4 to have a friend\nHas the same coloured mane as AJ", "\nDosent have one because she is a machine", "N/A", "Shadow Radon", "C418", 3, 0, "/images/null.png");
-	public static Ponii steven = new Ponii("Steven Stralanger", "\n", "\n", "Splater", "Plazza", 17, "/images/null.png");
+	public static Ponii c523 = new Ponii("C523 (C5)", "\nMare\nCream sort of colour\nAnother machine ponii built by radon however she was built for C4 to have a friend\nHas the same coloured mane as AJ", "\nDosent have one because she is a machine", "N/A", "Shadow Radon", "C418", 3, 0, "/images/C5.png");
+	public static Ponii steven = new Ponii("Steven Stralanger", "\n", "\n", "Splater", "Plazza", 17, "/images/steven.png");
 	
 	//Cannon poniis
 	public static Ponii SS = new Ponii("Sunset Shimmer", "\nMare\nUnicorn\nYou know from EQG?", "\nThe yellow and red sun thing", "?", "?", 19, "/imgages/null.png");
@@ -411,8 +411,6 @@ public class CharRP {
 			putValue(SHORT_DESCRIPTION, "Pushes string to internal system.");
 		}
 		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("button pressed");
-			System.out.println(lblTextArea.getText());
 			handConv(lblTextArea.getText());
 		}
 		

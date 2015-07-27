6adad25ae3ff0d35f293ee68ffbbd7318ec6f7ab
Reggie Peck <soulsilver771@gmail.com>
2015-07-27 15:32:29 -0600

    Ugh

--- src/DevTSK/Configurator/Config.java
+++ src/DevTSK/Configurator/Config.java
@@ -11,17 +11,26 @@ public class Config {
 	private static URI dir = null;
 	
 	public Config(int in) throws URISyntaxException, MalformedURLException {
-		init(in);
+		init(in, "");
 	}
 	
-	private static void init(int i) throws URISyntaxException, MalformedURLException{
+	public Config(String s) throws MalformedURLException, URISyntaxException {
+		init(0, s);
+	}
+	
+	private static void init(int i, String l) throws URISyntaxException, MalformedURLException{
 		String s = "null";
 		if (i == 1)
 			s = "file://./poniiconfig.ini";
 		
+		if (i == 0)
+			s = l;
+		
 		URL u = new URL(s);
 		
 		dir = u.toURI();
+		
+		System.out.println(dir);
 	}
 
 	public URI getDirectory() {
--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -1,13 +1,14 @@
 package development;
 
 import DevTSK.Configurator.Config;
+import DevTSK.Configurator.ConfigHandler;
 
 public class TestingEnv {
 
 	public static void main(String[] args) throws Exception {
 		Config c = new Config(1);
 		
-		System.err.println(c.getDirectory());
+		ConfigHandler ch = new ConfigHandler("file://laul");
 	}
 
 }

0322471ef75d07c97646f2752d223cd2ed01ef5a
The-Silver-Kid <regpuppy77@gmail.com>
2015-08-28 17:54:49 -0600

    laul

--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -1,12 +1,9 @@
 package development;
 
-import projectArchive.Pixl.WindowPixl;
-
 public class TestingEnv {
 
 	public static void main(String[] args) throws Exception {
-		WindowPixl w = new WindowPixl();
-		w.Menui.setVisible(true);
+		System.out.println("\u2708");
 	}
 
 }

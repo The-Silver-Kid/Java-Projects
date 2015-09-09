5a32d1b4e15da26465fca45e8b6b70b8cc6f776b
Reggie Peck <soulsilver771@gmail.com>
2015-09-09 14:13:30 -0600

    lol

--- src/pixl/Main.java
+++ src/pixl/Main.java
@@ -22,29 +22,39 @@ public class Main {
 		
 	}
 	
-	public static Graphics update(int[] gd, int[] bx, int[] by, Boolean[] ba, Boolean c) {
+	public static Graphics update(int[] gd, int[] bx, int[] by, Boolean[] ba, Boolean c, int gx, int gy) {
 		Graphics g = Host.GameFrame.getGraphics();
 		
 		g.create(0, 0, Host.GameFrame.getBounds().height, Host.GameFrame.getBounds().height);
 		
 		if (c)
-			g = u1(g, gd, bx, by, ba);
+			g = u1(g, gd, bx, by, ba, gx, gy);
 		if (!c)
-			g = u2(g, gd, bx, by, ba);
+			g = u2(g, gd, bx, by, ba, gx, gy);
 		
 		return g;
 	}
 	
-	private static Graphics u1(Graphics g, int[] gd, int[] bx, int[] by, Boolean[] ba) {
+	private static Graphics u1(Graphics g, int[] gd, int[] bx, int[] by, Boolean[] ba, int gx, int gy) {
 		g.setColor(SystemColor.window);
 		g.drawLine(gd[0], gd[1], gd[0], gd[1]);
+		g.drawLine(gx, gy, gx, gy);
+		for (int i = 0; i > bx.length; i++)
+			g.drawLine(bx[i], by[i], bx[i], by[i]);
 		
 		return g;
 	}
 	
-	private static Graphics u2(Graphics g, int[] gd, int[] bx, int[] by, Boolean[] ba) {
+	private static Graphics u2(Graphics g, int[] gd, int[] bx, int[] by, Boolean[] ba, int gx, int gy) {
 		g.setColor(new Color(0, 0, 255));
 		g.drawLine(gd[0], gd[1], gd[0], gd[1]);
+		g.setColor(new Color(0, 255, 0));
+		g.drawLine(gx-1, gy, gx+1, gy);
+		g.drawLine(gx, gy-1, gx, gy+1);
+		g.setColor(new Color(255, 0, 0));
+		for (int i = 0; i > bx.length; i++)
+			if (badAlive[i])
+				g.drawLine(bx[i], by[i], bx[i], by[i]);
 		
 		return g;
 	}
@@ -55,45 +65,45 @@ public class Main {
 	}
 
 	public static void Left() {
-		Host.GameFrame.setBackground(new Color(255, 255, 255));
-		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true));
-		Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true));
+		Host.Gameui.getContentPane().setBackground(new Color(0, 0, 0));
+		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
+		//Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
 		if (goodpos[0] > 1) 
 			goodpos[0] = goodpos[0] - 1;
-		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false));
+		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false, winx, winy));
 		handl();
 		check();
 	}
 
 	public static void Right() {
-		Host.GameFrame.setBackground(new Color(255, 255, 255));
-		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true));
-		Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true));
+		Host.Gameui.getContentPane().setBackground(new Color(0, 0, 0));
+		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
+		//Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
 		if (goodpos[1] < Host.GameFrame.getWidth()) 
 			goodpos[0] = goodpos[0] + 1;
-		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false));
+		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false, winx, winy));
 		handl();
 		check();
 	}
 	
 	public static void Down() {
-		Host.GameFrame.setBackground(new Color(255, 255, 255));
-		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true));
-		Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true));
+		Host.Gameui.getContentPane().setBackground(new Color(0, 0, 0));
+		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
+		//Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
 		if (goodpos[1] < Host.GameFrame.getWidth()) 
 			goodpos[1] = goodpos[1] + 1;
-		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false));
+		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false, winx, winy));
 		handl();
 		check();
 	}
 	
 	public static void Up() {
-		Host.GameFrame.setBackground(new Color(255, 255, 255));
-		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true));
-		Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true));
+		Host.Gameui.getContentPane().setBackground(new Color(0, 0, 0));
+		Host.Gameui.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
+		//Host.GameFrame.paint(update(goodpos, badposX, badposY, badAlive, true, winx, winy));
 		if (goodpos[1] > 1) 
 			goodpos[1] = goodpos[1] - 1;
-		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false));
+		Host.GameFrame.paintAll(update(goodpos, badposX, badposY, badAlive, false, winx, winy));
 		handl();
 		check();
 	}

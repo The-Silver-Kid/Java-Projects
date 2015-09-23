f9549b81ade1f3d9832459f44dfa097067e7bccd
Reggie Peck <soulsilver771@gmail.com>
2015-09-23 13:00:51 -0600

    lol

--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -1,9 +1,19 @@
 package development;
 
+import Class.BA;
+
 public class TestingEnv {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("\u2708");
+		BA ba = new BA(20);
+		System.out.println(ba.getBalance());
+		System.out.println("expected 20.0");
+		ba.deposit(5);
+		System.out.println(ba.getBalance());
+		System.out.println("expected 25.0");
+		ba.withdraw(5);
+		System.out.println(ba.getBalance());
+		System.out.println("expected 20.0");
 	}
 
 }

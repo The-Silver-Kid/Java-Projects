84d9ac87dbfc3c19146f3b20ebb6a36b2aa9ada9
Reggie Peck <soulsilver771@gmail.com>
2015-09-29 13:03:32 -0600

    jkdlaf;ds

--- src/development/Char/MarriedPoniiWithOtherKids.java
+++ src/development/Char/MarriedPoniiWithOtherKids.java
@@ -1,9 +1,46 @@
 package development.Char;
 
-public class MarriedPoniiWithOtherKids {
+public class MarriedPoniiWithOtherKids extends Ponii{
 
-	public MarriedPoniiWithOtherKids() {
-		// TODO Auto-generated constructor stub
+	protected String[] kids, with;
+	protected int kidammount;
+	protected String marriedto;
+	
+	public MarriedPoniiWithOtherKids(String Name, Boolean Gender, String Description, String ManeColour, String TailColour,
+			String Mother, String Father, int age, int month, int year, String Imagename, String CutiimarkImage,
+			String Marriedto, int ammountOfKids, String[] kidNames, String[] kidOtherParent) {
+		this.name = Name;
+		this.Gender = Gender;
+		this.description = Description;
+		this.Mane = ManeColour;
+		this.Tail = TailColour;
+		this.mother = Mother;
+		this.father = Father;
+		this.age = age;
+		this.month = month;
+		this.year = year;
+		this.imagepath = Imagename;
+		this.CutiiMarkPath = CutiimarkImage;
+		this.marriedto = Marriedto;
+		this.kidammount = ammountOfKids;
+		this.kids = kidNames;
+		this.with = kidOtherParent;
+	}
+	
+	public String getPartner(){
+		return marriedto;
+	}
+	
+	public String[] kidParents(){
+		return with;
+	}
+	
+	public int getKidAmmount(){
+		return kidammount;
+	}
+	
+	public String[] Kidnames(){
+		return kids;
 	}
 
 }
--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -1,13 +1,15 @@
 package development;
 
-import DevTSK.Util.*;
+import development.Char.Entity;
+import development.Char.Ponii;
 
 public class TestingEnv {
 
 	public static void main(String[] args) throws Exception {
-		BaseConv b = new BaseConv();
-		System.out.println(b.from10(50200, 16));
-		System.err.println(b.to10("C418", 16));
+		Entity e = new Entity();
+		System.err.println(e.getGender());
+		Ponii p = new Ponii();
+		System.err.println(p.getGender());
 	}
 
 }

a0c7d7994a753d5aeda9967ff880d7976e015532
The-Silver-Kid <regpuppy77@gmail.com>
2015-09-29 18:19:49 -0600

    gf

---
+++ src/development/Char/MasterControl.java
@@ -0,0 +1,20 @@
+package development.Char;
+
+import DevTSK.Char.Window;
+
+public class MasterControl {
+
+	private static final String[] commands = new String[] {
+			"Colour", "Color", "InputColour", "InputColor", "OutputColour", "OutputColor", "Music", "Exit",
+			"OutputTextColor", "InputTextColor", "OutputTextColour", "InputTextColour", "errorcheck" , "extract",
+			"breed", "breedrand", "last", "l", "lastcmd", "cfg", "config", "listNonOC", "listall"
+		};
+	
+	private static Window poni;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

969eef323acd9cdb05088dd76bce23a93ac860d4
The-Silver-Kid <regpuppy77@gmail.com>
2015-09-30 18:32:19 -0600

    y

--- src/development/Char/MasterControl.java
+++ src/development/Char/MasterControl.java
@@ -1,16 +1,14 @@
 package development.Char;
 
-import DevTSK.Char.Window;
-
 public class MasterControl {
 
-	private static final String[] commands = new String[] {
+	/*private static final String[] commands = new String[] {
 			"Colour", "Color", "InputColour", "InputColor", "OutputColour", "OutputColor", "Music", "Exit",
 			"OutputTextColor", "InputTextColor", "OutputTextColour", "InputTextColour", "errorcheck" , "extract",
 			"breed", "breedrand", "last", "l", "lastcmd", "cfg", "config", "listNonOC", "listall"
 		};
 	
-	private static Window poni;
+	private static Window poni;*/
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

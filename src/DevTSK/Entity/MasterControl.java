86ce3c4d316c83612419036be6b16c3da957eb56
The-Silver-Kid <regpuppy77@gmail.com>
2015-10-12 16:13:23 -0600

    asdfghjkl;'

--- src/DevTSK/Entity/MasterControl.java
+++ src/DevTSK/Entity/MasterControl.java
@@ -3,23 +3,23 @@ package DevTSK.Entity;
 import DAG.Config.ConfigException;
 
 public class MasterControl {
-	
+
 	public static void main(String[] args) throws ConfigException {
 		//Unmarried : Name, AltName, Flag, Gender, Description, ManeColour, TailColour, Mother, Father, Integer age, Integer day, Integer month, Integer year, ImageName, CutiimarkImage
 		EntityLoader h = new EntityLoader(new Entity[] {
-				new MarriedPoniiWithOtherKids("Shadow Radon", "Radon", "", false, "\nUnicorn\nYellow Thunderbolt accross underside\nYellow Thunderbolts arround hooves\nYellow and Black mane\nLight Industries Admin", "\nBlack Thundercloud with a yellow thunderbolt coming from it", "Yellow and Black", "Yellow and Black", "Charrie", "Slanger", 22, 20, 7, 1993, "Radon.png", "kloud.png", "Apple Jack", 3, new String[] {"Tree Lighting", "Shadow Jack", "Powder"}, new String[] {"Apple Jack", "Apple Jack", "Galecia Frostia"}),
-				new UnMarriedPoniiWithKids("Galecia Frostia", "Galecia", "", true, "\nUnicorn\nWas Radon's Marefriend for a while.... er, well still his marefreind.\nLikes the cold", "\nIcicle", "Light Blue", "Light Blue", "Krystla", "Starliner", 22, 20, 7, 1993, "Galecia.png", "null.png", 1, new String[] {"Powder"}, new String[] {"Shadow Radon"}),
-				new UnMarriedPoniiWithKids("Cosmos Darkstar","Cosmos","\u26e7",false,"\nZebra pegasus\nIs half demon and is also a vampony\nLives in the old castle in Everfree","\nPentagram","Purple with gray stripes","Gray","Nighmare Moon","King of Hell",851,21,8,1164 /*1, new String[] {""}, new String[] {""},*/,"null.png","null.png",1, new String[] {""}, new String[] {"Cadence"} ),
+				new MarriedPoniiWithOtherKids("Shadow Radon", "Radon", "", false, "\nUnicorn\nYellow Thunderbolt accross underside\nYellow Thunderbolts arround hooves\nYellow and Black mane\nLight Industries Admin", "\nBlack Thundercloud with a yellow thunderbolt coming from it", "Yellow and Black", "Yellow and Black", "Charrie", "Slanger", 22, 20, 7, 1993, "Radon.png", "kloud.png", "Apple Jack", 3, new String[] { "Tree Lighting", "Shadow Jack", "Powder" }, new String[] { "Apple Jack", "Apple Jack", "Galecia Frostia" }),
+				new UnMarriedPoniiWithKids("Galecia Frostia", "Galecia", "", true, "\nUnicorn\nWas Radon's Marefriend for a while.... er, well still his marefreind.\nLikes the cold", "\nIcicle", "Light Blue", "Light Blue", "Krystla", "Starliner", 22, 20, 7, 1993, "Galecia.png", "null.png", 1, new String[] { "Powder" }, new String[] { "Shadow Radon" }),
 				/*check this line*/
-				new UnMarriedPonii("Miiryanth","Miir", "",false,"\nDragon ponii\nI can haz dragon?", null /*He needs a new Class Delimiting the Cutii mark.*/,"Very dark gray","Firey orange-red color","Unknown","Unknown",19,4,8,1996,"null.png","null.png"),
+				new UnMarriedPoniiWithKids/*<-- You sure?*/("Cosmos Darkstar", "Cosmos", "\u26e7", false, "\nZebra pegasus\nIs half demon and is also a vampony\nLives in the old castle in Everfree", "\nPentagram", "Purple with gray stripes", "Gray", "Nighmare Moon", "King of Hell", 851, 21, 8, 1164 /*1, new String[] {""}, new String[] {""},*/, "null.png", "null.png", 1, new String[] { "" }, new String[] { "Cadence" }),
+				/*check this line*/
+				new UnMarriedPonii("Miiryanth", "Miir", "", false, "\nDragon ponii\nI can haz dragon?", null /*He needs a new Class Delimiting the Cutii mark.*/, "Very dark gray", "Firey orange-red color", "Unknown", "Unknown", 19, 4, 8, 1996, "null.png", "null.png"),
 		}, new Entity[] {
-				new MarriedPonii("Apple Jack Radon", "AJ", "", true, "\nEarth Ponii\nIts Apple Jack... y'all know her", "\nThree red apples", "Pale Yellow", "Pale Yellow", "?", "?", 22, 3, 4, 1993, "AJ.png", "AJMark.png", "Shadow Radon", 2, new String[] {"Tree Lighting", "Shadow Jack"}),
+				new MarriedPonii("Apple Jack Radon", "AJ", "", true, "\nEarth Ponii\nIts Apple Jack... y'all know her", "\nThree red apples", "Pale Yellow", "Pale Yellow", "?", "?", 22, 3, 4, 1993, "AJ.png", "AJMark.png", "Shadow Radon", 2, new String[] { "Tree Lighting", "Shadow Jack" }),
 				/*check this line*/
-				new UnMarriedPoniiWithKids/*<-- You sure?*/("Princess Cadence","Cadence","\u2764",true,"\nAlicorn\nPretty pink ponii princess","\nCrystal Heart","Pink as can be","Yellow, purple, and pink","Queen Galaxia","Unknown",11489,16,6,-9474,"null.png","null.png",/*"3547006110FFC7DAFFC49D00101740096UI1G3C002000000817F3FA2DF44A50Q107F3FCC004CB2"*/ 1,new String[] {""},new String[] {"Cosmos"}),
+				new UnMarriedPoniiWithKids/*<-- You sure?*/("Princess Cadence", "Cadence", "\u2764", true, "\nAlicorn\nPretty pink ponii princess", "\nCrystal Heart", "Pink as can be", "Yellow, purple, and pink", "Queen Galaxia", "Unknown", 11489, 16, 6, -9474, "null.png", "null.png", /*"3547006110FFC7DAFFC49D00101740096UI1G3C002000000817F3FA2DF44A50Q107F3FCC004CB2"*/ 1, new String[] { "" }, new String[] { "Cosmos" }),
 		});
 		h.punch();
 		h.setupConfig();
 		h.punch();
 	}
-
 }

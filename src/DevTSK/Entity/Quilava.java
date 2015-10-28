938898dd8f09648d691ddde9b87b909b33e1e8ac
Reggie Peck <soulsilver771@gmail.com>
2015-10-28 17:57:38 -0600

    l

---
+++ src/DevTSK/Entity/Quilava.java
@@ -0,0 +1,14 @@
+package DevTSK.Entity;
+
+public class Quilava extends Entity {
+
+	public Quilava() {
+	}
+
+	@Override
+	public String getInfo() {
+		return "A Quilava is... well you know.\n"
+				+ "Came to Equestria by some magic or another...\n"
+				+ "Radon might have something to do with that...";
+	}
+}

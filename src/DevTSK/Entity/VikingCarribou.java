ebedcf8dd526b4edfd25f4b7206e44ad64f26eef
Gabe Hill <cosmosdarkstarbrony@gmail.com>
2015-11-02 13:43:29 -0700

    maor infoooo

--- src/DevTSK/Entity/VikingCarribou.java
+++ src/DevTSK/Entity/VikingCarribou.java
@@ -15,7 +15,8 @@ public class VikingCarribou extends Entity {
 
 	@Override
 	public String getInfo() {
-		return "Carribou went extinct long ago because of their inability to mesh with other cultures, being marked as 'Vikings,' or 'Barbarians.'";
+		return "Carribou went extinct long ago because of their inability to mesh with other cultures, being marked as 'Vikings,' or 'Barbarians.'"
+				+ "For some reason they have short distance blinking (teleporting) powers.";
 
 	}
 }

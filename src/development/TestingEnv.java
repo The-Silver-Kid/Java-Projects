da361b7d80061055c0ed6cd0f684218d8c70cdff
Reggie Peck <soulsilver771@gmail.com>
2015-11-10 16:38:25 -0700

    asdfghjkl

--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -1,21 +1,7 @@
 package development;
 
-import DevTSK.Util.NumGenerator;
-
 public class TestingEnv {
 
-	public static final NumGenerator ng = new NumGenerator(20);
-
 	public static final void main(String args[]) {
-		int[][][][] i = new int[10][10][10][10];
-		for (int ii = 0; ii < 10; ii++) {
-			for (int iii = 0; iii < 10; iii++) {
-				for (int iiii = 0; iiii < 10; iiii++) {
-					for (int iiiii = 0; iiiii < 10; iiiii++)
-						System.out.println(ii + ":" + iii + ":" + iiii + ":" + iiiii + ":" + ng.gen());
-				}
-			}
-		}
 	}
-
 }

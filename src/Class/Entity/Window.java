6e5667bd99d3066ce1e810a99982cc619d50a164
Reggie Peck <soulsilver771@gmail.com>
2015-12-01 13:54:30 -0700

    asdfghj;'

--- src/Class/Entity/Window.java
+++ src/Class/Entity/Window.java
@@ -1,6 +1,7 @@
-/* 
- * Window Classgh
- * (C) DevTSK Productions 2015
+/**
+ * Window Class
+ *
+ * @author The_Silver_Kid
  */
 
 package Class.Entity;
@@ -28,7 +29,7 @@ import DevTSK.Exception.UnexceptableContentException;
 public class Window {
 	public JFrame frmPoniiPic;
 	public JFrame frmPoniiPicCont;
-	
+
 	public final Action action = new SwingAction();
 	public JLabel lblPoniiPic;
 	public JLabel lblCMPic;
@@ -37,10 +38,19 @@ public class Window {
 	public JScrollPane loltest;
 	public JButton in;
 	public String handler = "";
-	
-	public Window(String n, int close, int x, int y, int CharRPWin){
+
+	/**
+	 * Window Constructor
+	 *
+	 * @param n
+	 * @param close
+	 * @param x
+	 * @param y
+	 * @param CharRPWin
+	 */
+	public Window(String n, int close, int x, int y, int CharRPWin) {
 		if (CharRPWin == 0) {
-			
+
 			frmPoniiPic = new JFrame();
 			frmPoniiPic.getContentPane().setBackground(SystemColor.window);
 			frmPoniiPic.setIconImage(Toolkit.getDefaultToolkit().getImage(Window.class.getResource("/images/ikon.png")));
@@ -60,22 +70,22 @@ public class Window {
 					frmPoniiPic.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 				}
 			}
-			frmPoniiPic.getContentPane().setLayout(null);		
-			
+			frmPoniiPic.getContentPane().setLayout(null);
+
 			lblPoniiPic = new JLabel();
 			lblPoniiPic.setBounds(0, 0, 700, 700);
 			frmPoniiPic.getContentPane().add(lblPoniiPic);
-			
+
 			lblCMPic = new JLabel();
 			lblCMPic.setBounds(350, 0, 350, 350);
 			frmPoniiPic.getContentPane().add(lblCMPic);
-			
+
 			lblTextArea = new JTextField();
 			lblTextArea.setToolTipText("Ponii Name");
 			lblTextArea.setText("");
 			lblTextArea.setBounds(10, 710, 580, 20);
 			frmPoniiPic.getContentPane().add(lblTextArea);
-			
+
 			lblInfo = new JTextArea();
 			lblInfo.setWrapStyleWord(true);
 			lblInfo.setToolTipText("Information box");
@@ -84,23 +94,20 @@ public class Window {
 			lblInfo.setBounds(10, 740, 670, 120);
 			lblInfo.setEditable(false);
 			frmPoniiPic.getContentPane().add(lblInfo);
-			
+
 			loltest = new JScrollPane(lblInfo);
 			loltest.setBounds(10, 740, 670, 120);
 			loltest.setAutoscrolls(true);
 			frmPoniiPic.getContentPane().add(loltest);
-			
+
 			in = new JButton();
 			in.setBounds(600, 710, 80, 20);
 			in.setAction(action);
 			frmPoniiPic.getRootPane().setDefaultButton(in);
 			frmPoniiPic.getContentPane().add(in);
 		}
-		// TODO: WIP
-		//
-		//
 		if (CharRPWin == 1) {
-			
+
 			frmPoniiPic = new JFrame();
 			frmPoniiPic.getContentPane().setBackground(SystemColor.window);
 			frmPoniiPic.setIconImage(Toolkit.getDefaultToolkit().getImage(Window.class.getResource("/images/ikon.png")));
@@ -121,7 +128,7 @@ public class Window {
 				}
 			}
 			frmPoniiPic.getContentPane().setLayout(null);
-			
+
 			frmPoniiPicCont = new JFrame();
 			frmPoniiPicCont.getContentPane().setBackground(SystemColor.window);
 			frmPoniiPicCont.setIconImage(Toolkit.getDefaultToolkit().getImage(Window.class.getResource("/images/ikon.png")));
@@ -131,21 +138,21 @@ public class Window {
 			frmPoniiPicCont.setBounds(x + 700, y, 700, 500);
 			frmPoniiPicCont.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 			frmPoniiPicCont.getContentPane().setLayout(null);
-			
+
 			lblPoniiPic = new JLabel();
 			lblPoniiPic.setBounds(0, 0, 700, 700);
 			frmPoniiPic.getContentPane().add(lblPoniiPic);
-			
+
 			lblCMPic = new JLabel();
 			lblCMPic.setBounds(350, 0, 350, 350);
 			frmPoniiPic.getContentPane().add(lblCMPic);
-			
+
 			lblTextArea = new JTextField();
 			lblTextArea.setToolTipText("Ponii Name");
 			lblTextArea.setText("");
 			lblTextArea.setBounds(10, 10, 580, 20);
 			frmPoniiPicCont.getContentPane().add(lblTextArea);
-			
+
 			lblInfo = new JTextArea();
 			lblInfo.setWrapStyleWord(true);
 			lblInfo.setToolTipText("Information box");
@@ -154,12 +161,12 @@ public class Window {
 			lblInfo.setBounds(10, 40, 670, 420);
 			lblInfo.setEditable(false);
 			frmPoniiPicCont.getContentPane().add(lblInfo);
-			
+
 			loltest = new JScrollPane(lblInfo);
 			loltest.setBounds(lblInfo.getBounds());
 			loltest.setAutoscrolls(true);
 			frmPoniiPicCont.getContentPane().add(loltest);
-			
+
 			in = new JButton();
 			in.setBounds(600, 10, 80, 20);
 			in.setAction(action);
@@ -167,35 +174,64 @@ public class Window {
 			frmPoniiPicCont.getContentPane().add(in);
 		}
 	}
-	
+
+	/**
+	 * Changes the JLabel picture
+	 *
+	 * @param sr
+	 * @throws Exception
+	 */
 	private ImageIcon getImIcn(String sr) throws Exception {
 		Image img = ImageIO.read(Window.class.getResource(sr));
 		ImageIcon icn = new ImageIcon(img);
 		return icn;
 	}
-	
-	public void drawPic(String s) throws Exception{
+
+	/**
+	 * Draws the pic to the JLabel
+	 *
+	 * @param s
+	 * @throws Exception
+	 */
+	public void drawPic(String s) throws Exception {
 		lblPoniiPic.setIcon(getImIcn(s));
 	}
-	
+
+	/**
+	 * Prints the information given in s to the output window followed by the
+	 * init of a new line
+	 *
+	 * @param s
+	 */
 	public void println(String s) {
 		lblInfo.setText(lblInfo.getText() + s + "\n");
 	}
-	
+
+	/**
+	 * prints a empty line to the output textfield
+	 */
 	public void println() {
 		lblInfo.setText(lblInfo.getText() + "\n");
 	}
-	
+
+	/**
+	 * Clears the output text field
+	 */
 	public void printCl() {
 		lblInfo.setText("");
 	}
-	
+
+	/**
+	 * Starts the entire process of handling displaying and returning
+	 */
 	private class SwingAction extends AbstractAction {
 		private static final long serialVersionUID = 3646194311743048047L;
+
 		public SwingAction() {
 			putValue(NAME, "Get string");
 			putValue(SHORT_DESCRIPTION, "Pushes string to internal system.");
 		}
+
 		public void actionPerformed(ActionEvent arg0) {
 			try {
 				EntityLoader.handle(lblTextArea.getText());
@@ -203,9 +239,16 @@ public class Window {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
+	/**
+	 * Initiates the JLabel image change
+	 * 
+	 * @param imagePath
+	 * @return
+	 * @throws IOException
+	 */
 	public Icon getImageIcn(String imagePath) throws IOException {
 		Image img = ImageIO.read(Window.class.getResource(imagePath));
 		ImageIcon icn = new ImageIcon(img);

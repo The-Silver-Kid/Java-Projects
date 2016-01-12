253b7ca7712fe0bcbaaf0909e0e63a001ac6fc21
Reggie Peck <soulsilver771@gmail.com>
2016-01-12 13:54:48 -0700

    45

---
+++ src/Class/CH11/Reverse.java
@@ -0,0 +1,58 @@
+package Class.CH11;
+
+import java.util.ArrayList;
+
+/**
+ * This program replaces each line of a file with its reverse.
+ */
+public class Reverse {
+	/**
+	 * This method reads a file line by line and returns the result in an
+	 * array list.
+	 *
+	 * @param filename
+	 *            the file to read
+	 * @return an array list with the words in the file
+	 */
+	public static ArrayList<String> readLinesToArray(String filename) {
+		return null;
+
+	}
+
+	/**
+	 * Reverses a string.
+	 *
+	 * @param line
+	 *            the string to reverse
+	 * @return the reverse of line
+	 */
+	public static String reverse(String line) {
+		return "fic u";
+
+	}
+
+	/**
+	 * Reverses all elements of listOfLines.
+	 *
+	 * @param listOfLines
+	 *            the list of lines to reverse
+	 */
+	public static void reverseAll(ArrayList<String> listOfLines) {
+
+	}
+
+	/**
+	 * Write all lines in array list to given file.
+	 *
+	 * @param filename
+	 *            the name of the file to write to
+	 * @param lines
+	 *            the lines to write to the file
+	 */
+	public static void writeAll(String filename, ArrayList<String> lines) {
+	}
+
+	public static void main(String[] args) { // Filename is passed as command line argument{
+
+	}
+}

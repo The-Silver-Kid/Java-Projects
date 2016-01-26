40f4a321d87941a3a0f217247fe0f8340e1f0897
Reggie Peck <soulsilver771@gmail.com>
2016-01-26 14:10:53 -0700

    gyuera kjtuosfvn lfejtvyweo8uhriob yhoep;

--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -1,33 +1,15 @@
 package development;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class TestingEnv {
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) {
 
-		File f = new File("Input.txt");
-		if (!f.exists()) {
-			System.err.println("The file '" + args[0] + "' was not found!");
-			System.exit(1);
-		}
-
-		f.delete();
-		byte[] out = new byte[] { 'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D', '!' };
-		FileOutputStream fout = new FileOutputStream(f);
-		fout.write(out);
-		fout.close();
-
-		FileInputStream fin = new FileInputStream(f);
-		byte[] in = new byte[fin.available()];
-		fin.read(in);
-		fin.close();
-
-		for (int i = 0; i < in.length; i++)
-			System.out.print((char) in[i]);
+		System.out.println(fact(20));
 	}
 
+	public static long fact(int n) {
+		if (n == 1)
+			return 1l;
+		return n * fact(n - 1);
+	}
 }

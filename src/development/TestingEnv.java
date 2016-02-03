88b77f114f4978729c7be2bb4bd8b4b951dffc95
Reggie Peck <soulsilver771@gmail.com>
2016-02-03 14:01:38 -0700

    jfkdlsa;

--- src/development/TestingEnv.java
+++ src/development/TestingEnv.java
@@ -2,14 +2,15 @@ package development;
 
 public class TestingEnv {
 
-	public static void main(String args[]) {
-
-		System.out.println(fact(20));
+	public static void main(String[] args) {
+		main(new String[] {});
 	}
 
-	public static long fact(int n) {
-		if (n == 1)
-			return 1l;
-		return n * fact(n - 1);
+	public static long newFib(int n) {
+		if (n <= 3) {
+			return 1;
+		} else {
+			return newFib(n - 1) + newFib(n - 2) + newFib(n - 3);
+		}
 	}
 }
